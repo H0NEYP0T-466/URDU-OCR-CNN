b29a2c7a4cbdc93c04c1bb2af140826bfd83ce62
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-11-29 22:14:14 +0500

    Minnor Changes tomorrow i will do datasets and might train on some other day

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-/**
- * Main App Component
- * 
- * Root component with routing setup.
- */
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
